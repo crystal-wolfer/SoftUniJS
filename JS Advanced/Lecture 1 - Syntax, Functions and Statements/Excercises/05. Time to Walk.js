f8a6cae880311c8cacbacd9640b82899011b6a95
crystal-wolfer <cryptowolfer@gmail.com>
2023-09-14 20:18:29 +0300

    05. Update

--- JS Advanced/Lecture 1 - Syntax, Functions and Statements/Excercises/05. Time to Walk.js
+++ JS Advanced/Lecture 1 - Syntax, Functions and Statements/Excercises/05. Time to Walk.js
@@ -3,13 +3,13 @@ function timeToWalk(steps, footprint, speed){
 
   let speedConverted = speed * 5 / 18; // convert to m/s
   let distance = steps * footprint;
-  let addition = Math.floor(distance/500);
+  let addition = Math.round(distance/500);
   let time = (distance / speedConverted) + (addition*60);
   
 
   let hours = Math.floor(time / 3600);
   let minutes = Math.floor(time % 3600 / 60);
-  let seconds = Math.ceil(time % 3600 % 60);
+  let seconds = Math.round(time % 3600 % 60);
 
   // check if the number is  1 digit
   let hoursNew = hours < 10 ? `0${hours}` : hours; 
