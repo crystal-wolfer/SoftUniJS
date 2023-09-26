91f2f734aecbd7513c81caf73783a32d3a783c39
crystal-wolfer <97800053+crystal-wolfer@users.noreply.github.com>
2023-09-26 20:58:25 +0300

    06-08 Excercises

--- JS Advanced/Lecture 4 - DOM Introduction/Excercises/06. Format the Text/solution.js
+++ JS Advanced/Lecture 4 - DOM Introduction/Excercises/06. Format the Text/solution.js
@@ -1,3 +1,25 @@
 function solve() {
-  //TODO
+  let textInput = document.querySelector('#input').value;
+  let textArrayFiltered = Array.from(textInput.split('.').filter(elm => elm)); // filter out the empty strings
+
+
+  for (let i = 0; i < textArrayFiltered.length; i += 3) {
+    const p = document.createElement('p'); //creating a new <p> element
+    const node = document.createTextNode(`${textArrayFiltered[i]}.`); //add text to the <p> element, you must create a text node first. This code creates a text node
+    p.appendChild(node); // append the text node to the <p> element
+
+    if (textArrayFiltered[i + 1] !== undefined) { // validating the element excists and repeating the above process
+      const node2 = document.createTextNode(`${textArrayFiltered[i + 1]}.`);
+      p.appendChild(node2);
+    }
+
+    if (textArrayFiltered[i + 2] !== undefined) {
+      const node3 = document.createTextNode(`${textArrayFiltered[i + 2]}.`);
+      p.appendChild(node3);
+    }
+
+    const output = document.getElementById("output");
+    output.appendChild(p);
+  }
+
 }
--- JS Advanced/Lecture 4 - DOM Introduction/Excercises/07. Hell's Kitchen/app.js
+++ JS Advanced/Lecture 4 - DOM Introduction/Excercises/07. Hell's Kitchen/app.js
@@ -1,8 +1,71 @@
 function solve() {
    document.querySelector('#btnSend').addEventListener('click', onClick);
 
+   // capture elements
+   let input = document.querySelector('#inputs textarea');
+   let bestRestaurantResult = document.querySelector('#bestRestaurant p');
+   let workersResult = document.querySelector('#workers p');
+
+
+
    function onClick () {
-      //   TODO:
-      
+      let arr = JSON.parse(input.value);
+      let restaurants = {};
+
+      arr.forEach(element => {
+         let [name, workers] = element.split(' - ');
+         let workersCollections = workers.split(', ');
+
+         // crete dictionary collection for all workers an array of objects
+         let workersDictCollection = [];
+         for (let worker of workersCollections) {
+            let [workerName, salary] = worker.split(" ");
+            workersDictCollection.push({name: workerName, salary: Number(salary)});
+         }
+
+         // if the restaurant is already given: add the workers only into the workersDictCollection
+         if (restaurants[name]){
+            workersDictCollection = workersDictCollection.concat(restaurants[name].workers)
+         }
+
+         //sorting the workers dictionary by salary
+         workersDictCollection.sort((w1, w2) => w2.salary - w1.salary);
+
+         let bestSalary = workersDictCollection[0].salary;
+         let totalSalary = workersDictCollection.reduce((sum, w) => sum + w.salary, 0);
+         let avgSalary = totalSalary/workersDictCollection.length;
+
+         restaurants[name] = {
+            workers: workersDictCollection,
+            averageSalary: avgSalary,
+            bestSalary,
+         }
+
+      });
+
+      let bestRestaurantSalary = 0;
+      let bestRestaurant = undefined;
+
+      for (let name in restaurants) {
+         if (restaurants[name].averageSalary > bestRestaurantSalary){
+            bestRestaurant = {
+               name,
+               workers: restaurants[name].workers,
+               bestSalary: Number(restaurants[name].bestSalary),
+               averageSalary: restaurants[name].averageSalary,
+            }
+
+            bestRestaurantSalary = restaurants[name].averageSalary;
+         }
+
+      }
+
+      bestRestaurantResult.textContent = `Name: ${bestRestaurant.name} Average Salary: ${bestRestaurant.averageSalary.toFixed(2)} Best Salary: ${bestRestaurant.bestSalary.toFixed(2)}`
+
+      let resultWorkers = [];
+      bestRestaurant.workers.forEach(worker => resultWorkers.push(`Name: ${worker.name} With Salary: ${worker.salary}`));
+
+      workersResult.textContent = resultWorkers.join(' ');
+
    }
 }
