bb70e15db472e76292d9d93b52b046b9d7fd9e1d
crystal-wolfer <97800053+crystal-wolfer@users.noreply.github.com>
2023-10-01 18:03:32 +0300

    04-06 Excercise

--- JS Advanced/Lecture 5 - DOM Manipulations and Events/Excercises/04. Fill-Dropdown/dropdown.js
+++ JS Advanced/Lecture 5 - DOM Manipulations and Events/Excercises/04. Fill-Dropdown/dropdown.js
@@ -1,3 +1,23 @@
 function addItem() {
-    console.log('TODO:...');
+    // this is how to append a new item in the dropdown
+    //<option value="option1">Option1</option>
+
+    // capture elements
+    const addItemBtn = document.querySelector('input[type=button]');
+    const textField = document.querySelector('#newItemText');
+    const valueField = document.querySelector('#newItemValue');
+    const dropDownMenu = document.querySelector('#menu');
+
+
+    addItemBtn.addEventListener('click',addItemDropdown());
+
+    function addItemDropdown(){
+        let option = document.createElement('option');
+        option.value = valueField.value;
+        option.textContent = textField.value;
+        dropDownMenu.appendChild(option);
+
+        textField.value = '';
+        valueField.value = '';
+    }
 }
--- JS Advanced/Lecture 5 - DOM Manipulations and Events/Excercises/05. Encode-and-Decode-Messages/app.js
+++ JS Advanced/Lecture 5 - DOM Manipulations and Events/Excercises/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,40 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    //charCodeAt() to get the ASCII code; fromCharCode() to tur it into string
+
+    //The password for my bank account is  123pass321
+
+    //capture elements
+    const allBtns = document.querySelectorAll('div>button');
+    const encodeBtn = allBtns[0];
+    const decodeBtn = allBtns[1];
+    const allTextFields = document.querySelectorAll('div>textarea');
+    const messageField = allTextFields[0];
+    const decodeField = allTextFields[1];
+
+    encodeBtn.addEventListener('click', encodeHandler);
+    decodeBtn.addEventListener('click', decodeHandler);
+
+    function encodeHandler(){
+        let messageToEncode = messageField.value;
+        let encodedMessage = '';
+
+        for (let i = 0; i < messageToEncode.length; i++) {
+           encodedMessage += String.fromCharCode(messageToEncode.charCodeAt(i) + 1);
+        }
+
+        decodeField.value = encodedMessage;
+        messageField.value = '';
+    }
+
+    function decodeHandler() {
+        let messageToDecode = decodeField.value;
+        let decodedMessage = '';
+
+        for (let i = 0; i < messageToDecode.length; i++) {
+            decodedMessage += String.fromCharCode(messageToDecode.charCodeAt(i) - 1);
+        }
+
+        decodeField.value = decodedMessage; 
+    }
+ 
 }
