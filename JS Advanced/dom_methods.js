31ac51a6ead0000bdfc997c7fb6bdd269a501b55
crystal-wolfer <97800053+crystal-wolfer@users.noreply.github.com>
2023-10-11 11:26:58 +0300

    classes lab

---
+++ JS Advanced/dom_methods.js
@@ -0,0 +1,5 @@
+//element1.replaceWith(element2) - replaces element1 in HTML document with element2
+//element1.before(element2) - appends element1 in HTML document before element2 
+//element1.after(element2) - appends element1 in HTML document after element2
+//element1.hasAttribute('name') - method returns true if the specified attribute exists, otherwise it returns false
+//el1.dataset.myAttributeName = 10 - method that sets custom attribute to el1 in the DOM it will look like this: data-my-attribute-name="10" (transformed into kebabCase)
