14cfc1b0846f6668335e798b5840592e9fd35790
crystal-wolfer <97800053+crystal-wolfer@users.noreply.github.com>
2023-10-30 16:51:22 +0200

    05-06

--- JS Applications/02 Asynchronous Programming/Excercises/05. Accordion/accordion.js
+++ JS Applications/02 Asynchronous Programming/Excercises/05. Accordion/accordion.js
@@ -1,3 +1,101 @@
-function solution() {
-    //TODO .....
+window.addEventListener('load', solution);
+
+async function solution() {
+  //capture elements
+  const mainDiv = document.getElementById('main');
+
+  //function that creates the html section for each article
+  function generateSectionHTML(spanTitle, btnID, pMoreText) {
+    const accordionDiv = document.createElement("div");
+    accordionDiv.classList.add("accordion");
+
+    const headDiv = document.createElement("div");
+    headDiv.classList.add("head");
+
+    const spanElement = document.createElement("span");
+    spanElement.textContent = spanTitle;
+
+    const buttonElement = document.createElement("button");
+    buttonElement.classList.add("button");
+    buttonElement.id = btnID;
+    buttonElement.textContent = "More";
+    buttonElement.addEventListener("click", toggleMore)
+
+    headDiv.appendChild(spanElement);
+    headDiv.appendChild(buttonElement);
+
+    const extraDiv = document.createElement("div");
+    extraDiv.classList.add("extra");
+    extraDiv.style.display = "none";
+
+    const pElement = document.createElement("p");
+    pElement.textContent = pMoreText;
+
+    extraDiv.appendChild(pElement);
+
+    accordionDiv.appendChild(headDiv);
+    accordionDiv.appendChild(extraDiv);
+
+    return accordionDiv;
+  }
+
+  //fetching all the ids and keeping them in array
+  let arrId = [];
+  const fetchReq = (r) => {
+    if (r.status !== 200) {
+      throw new Error(`Server error: ${r.status}`);
+    }
+
+    return r;
+  };
+
+  try {
+    const baseURL = 'http://localhost:3030/jsonstore/advanced/articles/list'
+    const response = await fetch(baseURL, { method: 'GET' })
+    const fetchResult = fetchReq(response);
+    const arrEntries = await fetchResult.json();
+
+    //loop through the entries and create section for each entry
+    arrEntries.forEach(entry => {
+      arrId.push(entry._id);
+    });
+  }
+  catch (err) {
+    throw new Error(`Fetch Error: ${err}`);
+  }
+
+  // go through each id and create section for each entry
+  arrId.forEach(async (id) => {
+    try {
+      const URL = `http://localhost:3030/jsonstore/advanced/articles/details/${id}`;
+      const response = await fetch(URL, { method: 'GET' });
+      const fetchResult = fetchReq(response);
+      const itemObj = await fetchResult.json();
+      const itemId = itemObj._id;
+      const title = itemObj.title;
+      const content = itemObj.content;
+
+      const sectionDiv = generateSectionHTML(title, itemId, content);
+      mainDiv.appendChild(sectionDiv);
+    }
+    catch (err) {
+      throw new Error(`Fetch Error: ${err}`);
+    }
+  });
+
+  // define the toggleMore click event
+  function toggleMore(e){
+    const btn = e.target;
+    const divHidden = e.target.parentElement.parentElement.querySelector(".extra");
+
+    if (btn.textContent === "More"){
+      divHidden.style.display = "block";
+      btn.textContent = "Less";
+    } else{
+      divHidden.style.display = "none";
+      btn.textContent = "More";
+    }
+  }
+
+
 }
--- JS Applications/02 Asynchronous Programming/Excercises/06.Blog/app.js
+++ JS Applications/02 Asynchronous Programming/Excercises/06.Blog/app.js
@@ -1,5 +1,93 @@
 function attachEvents() {
-    console.log('TODO...');
-}
+  //capture elements
+  const btnLoadPosts = document.getElementById("btnLoadPosts");
+  const btnViewPost = document.getElementById("btnViewPost");
+  const selection = document.getElementById("posts");
+  const pTitleEl = document.getElementById("post-title");
+  const pBodyEl = document.getElementById("post-body");
+  const commentsUL = document.getElementById("post-comments");
 
+  // define the variables
+  let postsObj
+  let postIDs
+  let postTitles
+  const baseURL = 'http://localhost:3030/jsonstore/blog/';
+
+  //attach event handlers to the buttons
+  btnLoadPosts.addEventListener("click", loadPosts);
+  btnViewPost.addEventListener("click", showPost);
+
+  // fetch request handler
+  const fetchReq = (r) => {
+    if (r.status !== 200) {
+      throw new Error(`Server error: ${r.status}`);
+    }
+    return r;
+  };
+
+  async function loadPosts(e) {
+
+    try {
+      const URL = `${baseURL}posts`
+      const response = await fetch(URL, { method: "GET" });
+      const fetchResult = fetchReq(response);
+      postsObj = await fetchResult.json();
+
+      //extract the IDs and Titles
+      postTitles = Object.keys(postsObj).reduce((acc, key) => {
+        acc[key] = postsObj[key].title;
+        return acc;
+      }, {});;
+
+      // add the option to the selection list
+      for (const title in postTitles) {
+        const option = document.createElement('option');
+        option.textContent = postTitles[title].toUpperCase();
+        option.id = title;
+
+        selection.appendChild(option);
+      }
+    }
+    catch (err) {
+      throw new Error(`Fetch Error: ${err}`);
+    }
+  }
+
+  async function showPost(e) {
+    // clean previous inputs
+    pTitleEl.textContent = '';
+    pBodyEl.textContent = '';
+    commentsUL.innerHTML = '';
+
+    // get the selected post title, postBody & postId
+    const postId = selection.selectedOptions[0].id;
+    const postTitle = postsObj[postId].title;
+    const postBody = postsObj[postId].body;
+
+    try {
+      const URL = `${baseURL}comments`
+      const response = await fetch(URL, { method: "GET" });
+      const fetchResult = fetchReq(response);
+      const commentsObj = await fetchResult.json();
+
+      const commentsArr = Object.entries(commentsObj); // array of arrays index[0] is the id index[1] is the objects
+      const filtered = commentsArr.filter(([, obj]) => obj.postId === postId); // array of arrays index[0] is the id index[1] is the objects
+
+      pTitleEl.textContent = postTitle;
+      pBodyEl.textContent = postBody;
+
+      filtered.forEach(element => {
+        const li = document.createElement('li');
+        li.id = element[1].id;
+        li.textContent = element[1].text;
+
+        commentsUL.appendChild(li);
+      });
+
+    }
+    catch (err) {
+      throw new Error(`Fetch Error: ${err}`);
+    }
+  }
+}
 attachEvents();
