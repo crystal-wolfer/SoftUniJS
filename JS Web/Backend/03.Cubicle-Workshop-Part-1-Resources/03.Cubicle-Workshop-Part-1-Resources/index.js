10d3c5a088301edea13e4f145b7150b664f39e87
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-05-02 16:05:54 +0300

    CUB-3 Setup express to work static files & trim .hbs files

--- JS Web/Backend/03.Cubicle-Workshop-Part-1-Resources/03.Cubicle-Workshop-Part-1-Resources/index.js
+++ JS Web/Backend/03.Cubicle-Workshop-Part-1-Resources/03.Cubicle-Workshop-Part-1-Resources/index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const handlebars = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = 5000;
@@ -8,6 +9,10 @@ const PORT = 5000;
 app.engine('hbs', handlebars.engine({extname: 'hbs'}));
 app.set('view engine', 'hbs');
 app.set('views', 'src/views');
+
+//Setup static files
+const staticFiles = express.static(path.resolve(__dirname, "src/public")); // using the path lib to resolve path files like images and css files
+app.use(staticFiles)
 
 
 app.get('/', (req, res) => {
