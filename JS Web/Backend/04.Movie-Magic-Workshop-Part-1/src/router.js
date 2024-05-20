709378e39f7f2113494d944437405f0bc4a8c150
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-05-20 22:09:27 +0300

    CUB-16 Attach Cast & Small pages updates
    castManager, castController created and added to router

--- JS Web/Backend/04.Movie-Magic-Workshop-Part-1/src/router.js
+++ JS Web/Backend/04.Movie-Magic-Workshop-Part-1/src/router.js
@@ -2,9 +2,11 @@ const router = require('express').Router();
 
 const homeController = require('./controllers/homeController.js')
 const movieController = require('./controllers/movieController.js')
+const castController = require('./controllers/castController.js')
 
 router.use(homeController);
 router.use('/movies', movieController);
+router.use('/cast', castController);
 
 // every non-existent endpoint will redirect to the 404 endpoint
 router.get('*', (req, res) => {
