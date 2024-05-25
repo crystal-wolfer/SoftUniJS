2a26e47a4714ae5c8e7f0e7f39ab0b7aa988bc3f
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-05-25 16:40:33 +0300

    MOV-19 Refactor stars generation code
    movieController refactored the switch function with repeat fot the generation of stars in the rating

--- JS Web/Backend/04.Movie-Magic-Workshop-Part-1/src/controllers/movieController.js
+++ JS Web/Backend/04.Movie-Magic-Workshop-Part-1/src/controllers/movieController.js
@@ -10,15 +10,8 @@ router.get('/create', async (req, res) => {
 router.post('/create', (req, res) => {
   const { title,  genre,  director,  year,  imageUrl,  rating,  description } = req.body;
   //creating the additional stars property that will be keeping the symbols that will be displayed in the details page
-  let stars = '';
-  switch (rating) {
-    case '1': stars = '★'; break;
-    case '2': stars = '★★'; break;
-    case '3': stars = '★★★'; break;
-    case '4': stars = '★★★★'; break;
-    case '5': stars = '★★★★★'; break;
-    default: break;
-  }
+  const stars = '★'.repeat(Number(rating));
+
   movieManager.createMovie({ title,  genre,  director,  year,  imageUrl,  rating: Number(rating), stars,  description });
   res.redirect('/');
 });
@@ -39,7 +32,7 @@ router.get('/details/:movieId', async (req, res) => {
 router.get('/details/:movieId/add-cast', async (req, res) =>{
   const {movieId} = req.params
   const movie = await movieManager.getMovie(movieId);
-  const cast = await castManager.getNonAttachedCast(movie.cast);
+  const cast = await castManager.getNonAttachedCast(movie.cast); 
 
   res.render('./cast/attach', {movie, cast});
 });
