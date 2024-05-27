192da4c096a059d608e5cfcb5bc2126099d04149
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-05-28 01:00:03 +0300

    CUB-31 Edit func added
    edit cube

--- JS Web/Backend/03.Cubicle-Workshop-Part-1/src/services/cubeService.js
+++ JS Web/Backend/03.Cubicle-Workshop-Part-1/src/services/cubeService.js
@@ -1,41 +1,39 @@
-const uniqid = require('uniqid');
-const Cube = require('../models/Cube.js')
+const uniqid = require("uniqid");
+const Cube = require("../models/Cube.js");
 
 exports.createCube = async (cubeData) => {
   const newCube = await Cube.create(cubeData);
   return newCube;
-}
+};
 
 exports.getAllCubes = async (search, from, to) => {
   let filterCubes = await Cube.find().lean();
 
   //TODO: use mongoose to filter in the DB not in the server
-  if (search){
-    filterCubes = filterCubes.filter(c => c.name.toLowerCase().includes(search.toLowerCase()));
-  }
-
-  if (from){  
-    filterCubes = filterCubes.filter(c => 
-    c.difficultyLevel >= Number(from)
+  if (search) {
+    filterCubes = filterCubes.filter((c) =>
+      c.name.toLowerCase().includes(search.toLowerCase())
     );
   }
 
-  if (to){
-    filterCubes = filterCubes.filter(c => 
-    c.difficultyLevel <= Number(to)
-    );
+  if (from) {
+    filterCubes = filterCubes.filter((c) => c.difficultyLevel >= Number(from));
+  }
+
+  if (to) {
+    filterCubes = filterCubes.filter((c) => c.difficultyLevel <= Number(to));
   }
 
   return filterCubes;
 };
 
 exports.getCube = (id) => {
-  return Cube.findById(id); //the thing that uses this function should await it here it's simple so no need to await it because this is just a query; 
-}
+  return Cube.findById(id); //the thing that uses this function should await it here it's simple so no need to await it because this is just a query;
+};
 
 exports.getCubeWithAcc = (id) => {
-  return Cube.findById(id).populate('accessories'); //populate is integrated method that automatically poopulates all the data from the accessories table it's better to segrerate them
-}
+  return Cube.findById(id).populate("accessories"); //populate is integrated method that automatically poopulates all the data from the accessories table it's better to segrerate them
+};
 
 //attaching accessory to a cube
 exports.attachAcc = async (cubeId, accessoryId) => {
@@ -43,11 +41,16 @@ exports.attachAcc = async (cubeId, accessoryId) => {
   cube.accessories.push(accessoryId);
 
   return cube.save();
-}
+};
 
 // delete cube
 exports.deleteCube = async (cubeId) => {
   await Cube.findByIdAndDelete(cubeId);
-  return
+  return;
 };
- 
+
+// edit cube
+exports.editCube = async (cubeId, cubeData) => {
+  await Cube.findByIdAndUpdate(cubeId, cubeData);
+  return;
+};
