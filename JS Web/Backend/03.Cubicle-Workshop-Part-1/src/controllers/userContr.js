9b7870dfa625b89a61cefd84d5a49c2f069dccd7
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-05-28 11:13:15 +0300

    CUB-34 logout and coditional navigation
    authMiddleware set up locals for access to auth data and user

--- JS Web/Backend/03.Cubicle-Workshop-Part-1/src/controllers/userContr.js
+++ JS Web/Backend/03.Cubicle-Workshop-Part-1/src/controllers/userContr.js
@@ -1,33 +1,37 @@
-const router = require('express').Router();
-const userService = require('../services/userService.js')
-
+const router = require("express").Router();
+const userService = require("../services/userService.js");
 
 //REGISTER OPERATIONS
-router.get('/register', (req, res) => {
-  res.render('./userAuth/register')
-})
+router.get("/register", (req, res) => {
+  res.render("./userAuth/register");
+});
 
-router.post('/register', async (req, res) => {
-  const {username, password, repeatPassword} = req.body; 
+router.post("/register", async (req, res) => {
+  const { username, password, repeatPassword } = req.body;
 
-  await userService.register({username, password, repeatPassword});
-  res.redirect('/user/login');
-})  
-
+  await userService.register({ username, password, repeatPassword });
+  res.redirect("/user/login");
+});
 
 //LOGIN OPERATIONS
-router.get('/login', (req, res) => {
-  res.render('./userAuth/login')
-})
+router.get("/login", (req, res) => {
+  res.render("./userAuth/login");
+});
 
-router.post('/login', async(req, res) => {
-  const {username, password} = req.body;
+router.post("/login", async (req, res) => {
+  const { username, password } = req.body;
   const token = await userService.login(username, password);
 
   // setting up the cookie
-  res.cookie('userAuth', token, {httpOnly: true})
+  res.cookie("userAuth", token, { httpOnly: true });
 
-  res.redirect('/');
+  res.redirect("/");
+});
+
+// LOGOUT OPERATION
+router.get("/logout", async (req, res) => {
+  res.clearCookie("userAuth");
+  return res.redirect("/");
 });
 
 module.exports = router;
--- JS Web/Backend/03.Cubicle-Workshop-Part-1/src/middlewares/authMiddleware.js
+++ JS Web/Backend/03.Cubicle-Workshop-Part-1/src/middlewares/authMiddleware.js
@@ -9,6 +9,9 @@ exports.auth = async (req, res, next) => {
     try{
       const user = await jwt.verify(token, SECRET); // the payload
       req.user = user;
+      // set up authentication variable in the req obj that will be available in every request
+      res.locals.isAuthenticated = true;
+      res.locals.user = user;
       next();
     } catch (err){
       // in case the token is invalid
