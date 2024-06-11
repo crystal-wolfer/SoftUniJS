e0a611d8756f781465767b49367ae435b3b4453f
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-06-11 22:39:55 +0300

    React test set up with tailwind

---
+++ JS Web/React/React Test/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Feature from './components/Feature.jsx'
+import Hero from './components/Hero.jsx'
+import HowWorks from './components/HowWorks.jsx'
+import NavBar from './components/NavBar.jsx'
+
+function App() {
+
+  return (
+  <div>
+    <NavBar />
+     <Hero />
+     <Feature />
+     <HowWorks />
+  </div>
+  )
+}
+
+export default App
