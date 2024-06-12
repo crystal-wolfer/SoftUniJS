b8055e63d81eab6d309d89cc3eb653921888204d
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-06-12 13:06:38 +0300

    Nav and Hero section adjustments

--- JS Web/React/React Test/src/components/NavBar.jsx
+++ JS Web/React/React Test/src/components/NavBar.jsx
@@ -70,7 +70,7 @@ const Navbar = () => {
                 href="#"
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-700 md:dark:hover:text-primary-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Services
+                Market Overview
               </a>
             </li>
             <li>
@@ -78,7 +78,7 @@ const Navbar = () => {
                 href="#"
                 className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-primary-700 md:dark:hover:text-primary-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
               >
-                Contact
+                Login
               </a>
             </li>
           </ul>
