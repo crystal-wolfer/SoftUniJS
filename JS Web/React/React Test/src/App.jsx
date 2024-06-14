34feadb50f42950f01c3f788d8921b6acacc7065
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-06-14 18:21:22 +0300

    Updates
    small updates

--- JS Web/React/React Test/src/App.jsx
+++ JS Web/React/React Test/src/App.jsx
@@ -1,28 +1,27 @@
 import './App.css'
-// import NavBar from './components/NavBar.jsx'
-// import Hero from './components/Home Page/Hero.jsx'
-// import Market from './components/Home Page/Market.jsx'
-// import Feature from './components/Home Page/Feature.jsx'
-// import HowWorks from './components/Home Page/HowWorks.jsx'
-// import Customers from './components/Home Page/Customers.jsx'
-// import News from './components/Home Page/News.jsx'
-// import Footer from './components/Footer.jsx'
-import NotFound from './components/Static Pages/404.jsx'
+import NavBar from './components/NavBar.jsx'
+import Hero from './components/Home Page/Hero.jsx'
+import Market from './components/Home Page/Market.jsx'
+import Feature from './components/Home Page/Feature.jsx'
+import HowWorks from './components/Home Page/HowWorks.jsx'
+import Customers from './components/Home Page/Customers.jsx'
+import News from './components/Home Page/News.jsx'
+import Footer from './components/Footer.jsx'
+// import NotFound from './components/Static Pages/404.jsx'
 
 
 function App() {
 
   return (
   <div>
-    {/* <NavBar />
+    <NavBar />
      <Hero />
      <Market/>
      <Feature />
      <HowWorks />
      <Customers />
      <News />
-     <Footer/> */}
-     <NotFound></NotFound>
+     <Footer/>
   </div>
   )
 }
--- JS Web/React/React Test/src/components/Home Page/Hero.jsx
+++ JS Web/React/React Test/src/components/Home Page/Hero.jsx
@@ -11,7 +11,7 @@ export default function Hero() {
           </p>
           <a
             href="#"
-            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center text-white border-solid border-2 border-primary-800 rounded bg-primary-600 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
+            className="inline-flex items-center justify-center px-5 py-3 mr-3 text-base font-medium text-center btn-primary"
           >
             Get started
             <svg
