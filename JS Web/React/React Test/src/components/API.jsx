889fcf43343fca6a1b5f748d3e8d4881563646ce
t_yordanova <97800053+crystal-wolfer@users.noreply.github.com>
2024-07-14 21:15:45 +0300

    padding update

--- JS Web/React/React Test/src/components/API.jsx
+++ JS Web/React/React Test/src/components/API.jsx
@@ -55,7 +55,7 @@ export default function API() {
 
   return (
     <section className="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
-      <div className="py-8 max-w-screen-xl px-4 mx-auto 2xl:px-0">
+      <div className="py-20 max-w-screen-xl px-4 mx-auto 2xl:px-0">
         <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
           <div className="shrink-0 max-w-md lg:max-w-lg mx-auto">
           {dataFetched && (
